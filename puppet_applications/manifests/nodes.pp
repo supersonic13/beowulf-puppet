04b73dc7d2a36649f61948033e5bcfa6d32a218e
Arun Veluri <arun.veluri@bt.com>
2015-10-09 11:05:19 +0100

    Updated node name to Default

--- puppet_applications/manifests/nodes.pp
+++ puppet_applications/manifests/nodes.pp
@@ -1,5 +1,5 @@
 
-node 'beowulf-node' {
+node 'default' {
   # To create logrunner user and the working directories;
   # Disable if runnign on the live server as the users and the paths are already present;
   include environment    
