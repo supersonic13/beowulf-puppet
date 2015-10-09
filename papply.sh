b3ba6c8042714563f5fd04bb2fc692b6c1f77cbd
Arun Veluri <arun.veluri@bt.com>
2015-10-09 11:01:06 +0100

    Added gitignore

--- papply.sh
+++ papply.sh
@@ -2,5 +2,5 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $(dirname $(which puppet))
-./puppet apply -d $DIR/puppet-applications/manifests/site.pp --modulepath=$DIR/puppet-tools/modules/
+./puppet apply -d $DIR/puppet_applications/manifests/site.pp --modulepath=$DIR/puppet_tools/modules/
 cd -
--- puppet_applications/manifests/nodes.pp
+++ puppet_applications/manifests/nodes.pp
@@ -1,12 +1,12 @@
 
-node 'log-analyser-node' {
+node 'node3' {
   # To create logrunner user and the working directories;
   # Disable if runnign on the live server as the users and the paths are already present;
   include environment    
   
   #Required: The the analyser code in /opt/apps/log_analyser/repo/source_log-analyser/
-  include database
-  include httpserver
-  include log_analyser
+#  include database
+#  include httpserver
+#  include log_analyser
 
 }
