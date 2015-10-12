5b6bdc5b074ad64e782b0cfbd8126c436224e6cd
Arun Veluri <arun.veluri@bt.com>
2015-10-12 11:52:46 +0100

    Node updates bassed on master slave

--- puppet_applications/manifests/nodes.pp
+++ puppet_applications/manifests/nodes.pp
@@ -1,6 +1,13 @@
 
 node 'default' {
   
+  if $hostname =~ /beowulf-master/ {
+    include environment::master
+  }
+  else
+  {
+    include environment::slave
+  }
   #include environment
   #include environment::master
   #include environment::slave
